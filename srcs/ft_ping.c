363fd0604e5900722feac9da1bd120d2a910185a
pzgm <pizza@gami.pz>
2023-03-06 10:08:12 +0100

    like new

--- srcs/ft_ping.c
+++ srcs/ft_ping.c
@@ -6,21 +6,21 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-	struct addrinfo res;
+	struct addrinfo *res;
 	struct sockaddr_in dest_addr;
 	struct timeval start_time;
 
-	get_addr(argv[1], &res);
+	char * ip_str = get_addr(argv[1], &res);
 	memset(&dest_addr, 0, sizeof(dest_addr));
 
-	int sockfd = socketfd(&res, &dest_addr);
+	int sockfd = socketfd(res, &dest_addr);
 
 	int seq = 0;
 
 	while (++seq) {
 
 		send_ping(sockfd, &dest_addr,seq, &start_time);
-		recv_ping(sockfd, &dest_addr, seq, &start_time);
+		recv_ping(sockfd, &dest_addr, seq, &start_time, argv[1], ip_str);
 
 		sleep(1);
 	}
